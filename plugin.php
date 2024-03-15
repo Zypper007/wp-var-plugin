e7af08f2daa37548dcb0221f51d0e9339cb9dbf0
Patryk Dumin <57997146+Zypper007@users.noreply.github.com>
2024-03-15 08:07:51 +0100

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Zmienne
+Plugin URI: dumino.pl/wp/plugins/varaibles
+Description: Umożliwia korzystanie z zmiennych w edytorze
+Author: Patryk Dumin
+Version: 1.0
+*/
+
+
+?>
